c66635353078eee3abe04a2eac1f0cd13eff04c0
Josh Karpel <karpel@wisc.edu>
2020-05-08 15:32:28 -0500

    augment python path

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,12 @@
 import logging
 import os
+import sys
 
 HERE = os.path.dirname(__file__)
+sys.path.append(HERE)
 
 logging.basicConfig(filename=os.path.join(HERE, "logs.txt"), level=logging.DEBUG)
 
-
 from registry import create_app
-
 
 application = create_app()
