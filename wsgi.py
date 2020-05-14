6cd6b636982c384348230eca4e4931a40528b664
Josh Karpel <josh.karpel@gmail.com>
2020-05-14 10:21:27 -0500

    use a more robust logging config (resolve #14)

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,32 @@
-import logging
+from logging.config import dictConfig
 import os
 import sys
 
 HERE = os.path.dirname(__file__)
 sys.path.append(HERE)
 
-logging.basicConfig(filename=os.path.join(HERE, "logs.txt"), level=logging.DEBUG)
+dictConfig(
+    {
+        "version": 1,
+        "formatters": {
+            "default": {
+                "format": "[%(asctime)s] [%(levelname)s] %(module)s:%(lineno)s ~ %(message)s",
+            }
+        },
+        "handlers": {
+            "file": {
+                "class": "logging.handlers.RotatingFileHandler",
+                "level": "DEBUG",
+                "formatter": "default",
+                "filename": os.path.join(HERE, "logs/registration.log"),
+                "maxBytes": 10485760,
+                "backupCount": 5,
+            },
+        },
+        "root": {"level": "DEBUG", "handlers": ["file"]},
+    }
+)
+
 
 from registry import create_app
 
