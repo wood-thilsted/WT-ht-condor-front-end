859dadad5f1265fe17e6417e223359dd9ba404c4
Jason Patton <jpatton@cs.wisc.edu>
2020-06-24 16:38:00 -0500

    Add macOS with Docker install instructions

--- registry/connect/views.py
+++ registry/connect/views.py
@@ -49,3 +49,27 @@ def ubuntu_18():
 
     response = make_response(render_template("ubuntu-18.html", **context))
     return response
+
+
+@install_bp.route("/connect/macos", methods=["GET"])
+def macos():
+    try:
+        user_id = get_user_id()
+    except ConfigurationError:
+        return "Server configuration error", 500
+
+    sources = get_sources(user_id)
+
+    install_commands = {
+        source: "bash install_htcondor.sh -c {} -n {} -d {} -x Docker".format(
+            current_app.config["COLLECTOR"],
+            source,
+            current_app.config["DEFAULT_DATA_DIRECTORY"],
+        )
+        for source in sources
+    }
+
+    context = {"sources": sources, "install_commands": install_commands}
+
+    response = make_response(render_template("macos-docker.html", **context))
+    return response
