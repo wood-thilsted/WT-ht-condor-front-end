c15e2b1a7881c98edd42cb0fdd9cfe24d14b4f27
Brian Bockelman <bbockelm@cse.unl.edu>
2020-11-11 22:58:21 -0600

    Add example config files

---
+++ examples/config.py
@@ -0,0 +1,7 @@
+
+SUPPORT_EMAIL="example@morgridge.org"
+USER_ID_ENV_VAR = "REMOTE_USER"
+USER_ID_FAKE = "example@wisc.edu"
+HUMANS_FILE = "humans.ini"
+COLLECTOR = "flock.opensciencegrid.org"
+DEFAULT_DATA_DIRECTORY = "/srv"
